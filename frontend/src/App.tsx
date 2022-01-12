1a564e4d6bde0b34b138dc292742fd0c7f4c1c4c
allisonvinicius <allisonvinicius63@gmail.com>
2022-01-11 22:37:12 -0400

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-import Navbar from "Components/Navbar";
 
+import Navbar from "Components/Navbar";
+import Form from 'pages/Form';
+import Listing from 'pages/Listing';
+import {
+  BrowserRouter, Route, Routes
+} from "react-router-dom";
 
 
 function App() {
   return (
-  <Navbar />
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
