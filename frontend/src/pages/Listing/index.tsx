2d4793b9f3361e51f55412235ef0e9bc96af83fe
allisonvinicius <allisonvinicius63@gmail.com>
2022-01-18 20:56:12 -0400

    insert hooks project

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,13 +1,30 @@
 
+import { axios } from 'axios';
 import MovieCard from "Components/MovieCard";
 import Pagination from "Components/Pagination";
+import React, { useEffect, useState } from 'react';
+import { MoviePage } from 'types/movie';
+import { BASE_URL } from 'utils/requests';
 
 
 
 function Listing (){
 
+        const [pageNumber, setPageNumber] = useState(0);
+
+        useEffect(() => {
+            axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPageNumber(data.number);
+        });
+        },[]);
+
+        
+
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
             <div className="row">
